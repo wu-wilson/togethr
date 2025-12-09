73ec2ef62e9828089567bba1911d00c77426a99c
wu-wilson <wilson.cui.wu@gmail.com>
2025-12-09 16:28:49 -0600

    initialized shadcn

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from "vite";
+import path from "path";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [tailwindcss(), react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
