5a28516c2fd2e429b6d60546212b062240df1c6d
wu-wilson <wilson.cui.wu@gmail.com>
2025-12-09 18:23:40 -0600

    removed unused import

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,3 @@
-import { ThemeToggle } from "./components/custom/theme-toggle";
 import { ThemeProvider } from "./contexts/theme-provider/theme-provider";
 import NavigationBar from "./components/custom/navigation-bar";
 
