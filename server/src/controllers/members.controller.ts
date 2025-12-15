261eed8306ce6244290879dc8fc535e6395d8ac3
wu-wilson <wilson.cui.wu@gmail.com>
2025-12-15 09:54:27 -0600

    added health checks to db, implemented get members endpoint

--- server/src/controllers/members.controller.ts
+++ server/src/controllers/members.controller.ts
@@ -1,5 +1,17 @@
 import { Request, Response } from "express";
+import { pool } from "../index.js";
 
-export const getMembers = (req: Request, res: Response) => {
-  res.json([]);
+export const getMembers = async (req: Request, res: Response) => {
+  try {
+    const { rows: members } = await pool.query(
+      `
+      SELECT *
+      FROM members
+      `
+    );
+    res.json(members);
+  } catch (error) {
+    console.error("Error fetching members:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
 };
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,3 +1,4 @@
+import { Pool } from "pg";
 import membersRouter from "./routers/members.router.js";
 import express from "express";
 import cors from "cors";
@@ -11,10 +12,28 @@ app.use(
 );
 app.use(express.json());
 
+export const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  max: 20,
+});
+
+const testDbConnection = async () => {
+  try {
+    const client = await pool.connect();
+    console.log("Database connected...");
+    client.release();
+  } catch (err) {
+    console.error("Error acquiring client", err);
+    process.exit(1);
+  }
+};
+
+testDbConnection();
+
 app.use("/members", membersRouter);
 
 const PORT = 5000;
 
 app.listen(PORT, () => {
-  console.log(`listening on port ${PORT}...`);
+  console.log(`Listening on port ${PORT}...`);
 });
