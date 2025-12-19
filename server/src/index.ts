ee76d8e1102689a0aca16d925ac12f0c5c97a2b5
wu-wilson <wilson.cui.wu@gmail.com>
2025-12-19 14:40:10 -0600

    updated cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,7 +7,7 @@ import cors from "cors";
 const app = express();
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000", "http://127.0.0.1:3000"],
     credentials: true,
   })
 );
