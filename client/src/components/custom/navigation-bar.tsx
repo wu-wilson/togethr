d4a51f2e161aaa65c4e6f39b2c9641854152b370
wu-wilson <wilson.cui.wu@gmail.com>
2025-12-26 17:45:30 -0500

    cleaned up nav bar component

--- client/src/components/custom/navigation-bar.tsx
+++ client/src/components/custom/navigation-bar.tsx
@@ -1,32 +1,14 @@
 import { ThemeToggle } from "./theme-toggle";
-import { Button } from "@/components/ui/button";
-import { HeartHandshake } from "lucide-react";
 import { Separator } from "@/components/ui/separator";
 import DateRangePicker from "./date-range-picker";
 
 const NavigationBar = () => {
-  const sections = ["Dashboard", "Transactions", "People"];
-
   return (
     <div className="flex justify-center border-b sticky top-0 z-50 bg-background px-6 h-16">
-      <div className="flex justify-center md:justify-between items-center max-w-7xl w-full">
-        <div className="items-center gap-1 hidden md:flex">
-          <Button variant="ghost" size="icon">
-            <HeartHandshake className="size-5" />
-          </Button>
-          <>
-            {sections.map((s) => (
-              <Button key={s} size="sm" variant="ghost">
-                {s}
-              </Button>
-            ))}
-          </>
-        </div>
-        <div className="flex items-center">
-          <DateRangePicker />
-          <Separator orientation="vertical" className="ml-3.5 mr-1 my-1" />
-          <ThemeToggle />
-        </div>
+      <div className="flex justify-end items-center max-w-7xl w-full">
+        <DateRangePicker />
+        <Separator orientation="vertical" className="ml-3.5 mr-1 my-4" />
+        <ThemeToggle />
       </div>
     </div>
   );
